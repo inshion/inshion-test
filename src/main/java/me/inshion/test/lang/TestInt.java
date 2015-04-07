ea61d7129bf9cda09f7d44521b06c76a204abe26
inshion <inshion@vip.qq.com>
2015-04-07 17:40:52 +0800

    test

---
+++ src/main/java/me/inshion/test/lang/TestInt.java
@@ -0,0 +1,16 @@
+package me.inshion.test.lang;
+
+public class TestInt {
+
+	public static void main(String[] args) {
+		System.out.println(Integer.MAX_VALUE);
+//		Integer a = 11;
+//		f(a);
+
+		System.out.println(Character.SIZE);
+	}
+
+	static <T> T f(long a) {
+		return null;
+	}
+}
---
+++ src/main/java/me/inshion/test/vm/PTest.java
@@ -0,0 +1,9 @@
+package me.inshion.test.vm;
+
+
+public class PTest {
+
+	public static void main(String[] args) {
+		
+	}
+}
